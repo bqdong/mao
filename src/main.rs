14bbe98bdb990648d1e0f50b97b779b8f0302dc1
bqdong <bqdong137@gmail.com>
2024-07-04 23:44:25 +0800

    demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process::ExitCode;
+
+#[cfg(target_os = "linux")]
+fn main() -> ExitCode {
+    for argument in env::args() {
+        println!("{argument}");
+    }
+
+    let key = "HOME";
+    match env::var(key) {
+        Ok(val) => println!("{key}: {val:?}"),
+        Err(e) => println!("error {key}, {e}"),
+    }
+
+    ExitCode::SUCCESS
 }
