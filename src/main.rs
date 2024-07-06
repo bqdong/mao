a18efa3804a158786d90b0f20b656d323fe0c4e4
bqdong <bqdong137@gmail.com>
2024-07-06 19:32:02 +0800

    file info

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,10 @@ fn main() {
     let path = cmd_args.nth(1).unwrap();
     let fd = fs::open(path, fs::OFlags::RDONLY, fs::Mode::empty()).unwrap();
     let stat = fs::fstat(fd).unwrap();
-    print!("{stat:?}");
+    println!("{stat:?}");
+    println!();
+    println!("Device: {0}", stat.st_dev);
+    println!("Link Num: {0}", stat.st_nlink);
+    let file_type = fs::FileType::from_raw_mode(stat.st_mode);
+    println!("File Type: {file_type:?}");
 }
